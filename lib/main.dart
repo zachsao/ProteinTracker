6522900efa648743cb48e44a1217d89de6c5a5a3
zach sao <zakaria.sao@gmail.com>
2023-08-10 22:43:36 -0400

    Add firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:protein_tracker/color_schemes.g.dart';
@@ -7,6 +9,13 @@ import 'package:protein_tracker/widget_tree.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const PeachyApp());
 }
 
@@ -19,7 +28,9 @@ class PeachyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: lightColorScheme,
       ),
-      home: const Scaffold(body: WidgetTree(),),
+      home: const Scaffold(
+        body: WidgetTree(),
+      ),
     );
   }
 }
